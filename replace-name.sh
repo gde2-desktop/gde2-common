94af3d95e7089869098b13cb9caed9f306cd5df1
Михалыч <gtk3@inbox.ru>
2020-11-29 10:28:08 +0500

    Обновлена утилита автопереименования имён программ

--- replace-name.sh
+++ replace-name.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+# Кутный Максим <gtk3@inbox.ru>
 
-srcdir=`dirname $0`
+srcdir="$1"
 find "$srcdir" -type f -exec \
-sed -i 's/GDE2/GDE2/g' {} + -print
+sed -i 's/MATE/GDE2/g' {} + -print
 
 find "$srcdir" -type f -exec \
-sed -i 's/gde2/gde2/g' {} + -print
+sed -i 's/mate/gde2/g' {} + -print
+
+find "$srcdir" -type f -exec \
+sed -i 's/Mate/Gde2/g' {} + -print
+
+find "$srcdir" -type f -execdir rename mate gde2 '{}' +
+rename mate gde2 "$srcdir"/*
